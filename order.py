e508b5dacdd06c2fa2b9c94295cd4a44fa7c87fd
Ngecha <ngechadennis@gmail.com>
2024-09-14 09:05:22 +0300

    assignment

--- order.py
+++ order.py
@@ -1,11 +1,9 @@
 class Order:
     def __init__(self, customer, coffee, price):
         # Initializes the Order with a customer, coffee, and price
-        self.customer = customer  # Sets the customer for this order
-        self.coffee = coffee  # Sets the coffee for this order
-        self.price = price  # Sets the price for this order
-
-        # Adds this order to the customer's and coffee's list of orders
+        self.customer = customer  
+        self.coffee = coffee  
+        self.price = price  
         customer.orders().append(self)  
         coffee.orders().append(self)  
 
@@ -17,7 +15,6 @@ class Order:
     @customer.setter
     def customer(self, value):
         # Setter for the customer attribute with validation
-        # Ensures that the customer is an instance of the Customer class
         from .customer import Customer  
         if isinstance(value, Customer):
             self._customer = value
@@ -32,7 +29,6 @@ class Order:
     @coffee.setter
     def coffee(self, value):
         # Setter for the coffee attribute with validation
-        # Ensures that the coffee is an instance of the Coffee class
         from .coffee import Coffee  
         if isinstance(value, Coffee):
             self._coffee = value
@@ -47,7 +43,6 @@ class Order:
     @price.setter
     def price(self, value):
         # Setter for the price attribute with validation
-        # Ensures that the price is a float between 1.0 and 10.0
         if isinstance(value, float) and 1.0 <= value <= 10.0:
             self._price = value
         else:
